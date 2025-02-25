ea9dbb864284bff7ab139dea7b8d3a350c27fbcd
Precious Onu (Arnold) <Poarnold@outlook.com>
2025-02-25 14:11:50 +0000

    update the function description

--- hng_task_1.py
+++ hng_task_1.py
@@ -1,53 +1,63 @@
 from flask import Flask, request, jsonify
-import requests
+import requests # to access external apis
+from flask_cors import CORS #to allow cross origin resource sharing
 
 app = Flask(__name__)
 app.json.sort_keys=False
+CORS(app)
 
-# @app.errorhandler(400)
-# def alphabet_passed():
-#     is_error = True
-#     response = jsonify({'number': 'alphabet',
-#                 'error': is_error})
-    
-#     return response, 400
 
 @app.route('/')
 def welcome():
+    # Default Homepage response for the application endpoint
     return 'HNG Backend Stage 1 Task'
 
 
 @app.route('/api/classify-number', methods = ['GET'])
 def classify_num():
     '''
-    Performs three tests:
+    This Flask Function returns the properties of a positive integer entry. Valid inputs are positive integers. 
+    The function performs four tests on the inputs and returns their properties.
         i. prime test
         ii. perfect test
         iii. armstrong test
         iv. parity test
-    
-    Returns:
-        i. The proprties of the number from the test
-        ii. A funfact from number api
-    '''
+        
+    A basic error check is implemented to return response when a different object type is inputted.
+        
+    Parameters
+    ----------
+        number (int): positive integer
+    Returns
+    -------
+         json,  {"number": input num,
+                "is_prime": prime test,
+                "is_perfect": perfect test,
+                "properties": [armstrong test, parity test],
+                 "digit_sum": sum of digits,
+                "fun_fact": response from <http://numbersapi.com/'+str(input num)+'/math?json>}
+
+    ''' 
     input_num = request.args.get('number', type=int) #Variable to store input
     
     try:
         
         input_num = int(input_num)
 
+        is_positive = 1
         if input_num < 0:
-            raise ValueError("Negative Number")
+            is_positive = 0
+            input_num = abs(input_num)
 
         ## TODO-1: Prime test - input number is positive, greater than 0
         divisors = []
         is_prime = False #assume a number is not a prime number
         for divisor in range(1,input_num):
-            if input_num%divisor == 0:
+            if input_num%divisor == 0 and is_positive:
                 divisors.append(divisor)
             # if len(divisors) > 3:
             #         break
-        if len(divisors) < 2:
+        if len(divisors) < 2 and len(divisor) != 0:
             is_prime = True
         
         
@@ -64,10 +74,11 @@ def classify_num():
         digit_sum = 0
         arm_sum = 0
         is_armstrong = False
+        
         for n in arm_num:
             arm_sum += int(n)**len_num
             digit_sum += int(n)
-        if arm_sum == input_num:
+        if arm_sum == input_num and is_positive:
             is_armstrong = True
 
 
@@ -109,26 +120,13 @@ def classify_num():
             return response, 400
     
     except ValueError:
-            is_error = True
-            response = jsonify({'number': 'negative',
-                                'error': is_error})
-            
-            return response, 400
+        is_error = True
+        response = jsonify({'number': 'negative',
+                    'error': is_error})
+        
+        return response, 400
 
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
     
-
-
-
-    
-    
-
-
-
-
-
-
-
